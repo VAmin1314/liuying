6cc75420b2ba0423abd3dbcdf5396088da3d8a28
LiuJian <574996079@qq.com>
2017-03-05 23:43:41 +0800

    x

--- database/migrations/2017_03_05_140623_create_table_photo.php
+++ database/migrations/2017_03_05_140623_create_table_photo.php
@@ -18,6 +18,8 @@ class CreateTablePhoto extends Migration
             $table->integer('admin_id')->comment('添加图片的id');
             $table->string('path')->default('')->comment('图片的路径');
             $table->string('title')->default('')->comment('图片的大标题');
+            $table->string('shot_time')->default('')->comment('图片的拍摄时间');
+            $table->string('shot_add')->default('')->comment('图片的拍摄地点');
             $table->string('little_title')->default('')->comment('图片的小标题');
             $table->text('description')->comment('图片的描述');
 
--- resources/views/admin/issue_photo.blade.php
+++ resources/views/admin/issue_photo.blade.php
@@ -31,3 +31,58 @@
     });
 </script>
 @endsection
+
+@section('main')
+<section class="panel">
+    <header class="panel-heading">
+        图片发布
+    </header>
+    <div class="panel-body">
+        <form class="form-horizontal tasi-form" method="get">
+            <div class="form-group">
+                <label class="col-sm-2 control-label">标题</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">小标题</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control">
+                    <span class="help-block">介绍</span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">描述</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control round-input">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Disabled</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="disabledInput" type="text" placeholder="Disabled input here..." disabled="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Placeholder</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" placeholder="placeholder">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-2 control-label">Static control</label>
+                <div class="col-lg-10">
+                    <p class="form-control-static">email@example.com</p>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+@endsection
