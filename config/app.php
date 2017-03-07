6732aa996d15c803f0a724f22515346fa8167d33
LiuJian <574996079@qq.com>
2017-03-07 19:55:52 +0800

    x

--- config/app.php
+++ config/app.php
@@ -177,8 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Illuminate\Support\Facades\Redis::class,
-
     ],
 
     /*
@@ -227,9 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-
     ],
 
 ];
