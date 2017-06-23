a1fd2d092e2974f7f3b165b09cb3f2f1d3a638d7
liujian <574996079@qq.com>
2017-06-24 01:04:58 +0800

    x

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,5 @@
 <?php
-
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,21 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('admin')->insert([
-        //     'name' => '小屁孩',
-        //     'password' => '123456',
-        //     'created_at' => '2017-01-01 00:00:00',
-        //     'updated_at' => '2017-01-01 00:00:00'
-        // ]);
-        for ($i = 1000; $i < 500000000; $i++) {
-            DB::table('setting')->insert([
-                'key' => 'setting'.$i,
-                'value' => '',
-                'created_at' => '2017-01-01 00:00:00',
-                'updated_at' => '2017-01-01 00:00:00'
-            ]);
-            echo $i.PHP_EOL;
-        }
+        DB::table('admin')->insert([
+            'name' => 'admin',
+            'password' => Hash::make('123456'),
+            'created_at' => '2017-01-01 00:00:00',
+            'updated_at' => '2017-01-01 00:00:00'
+        ]);
+        DB::table('setting')->insert([
+            'key' => 'setting',
+            'value' => '{"is_allow":"1","autoplay":"1"}',
+            'created_at' => '2017-01-01 00:00:00',
+            'updated_at' => '2017-01-01 00:00:00'
+        ]);
+        // for ($i = 1000; $i < 500000000; $i++) {
+        //     DB::table('setting')->insert([
+        //         'key' => 'setting'.$i,
+        //         'value' => '',
+        //         'created_at' => '2017-01-01 00:00:00',
+        //         'updated_at' => '2017-01-01 00:00:00'
+        //     ]);
+        //     echo $i.PHP_EOL;
+        // }
         echo 'end';
     }
 }
